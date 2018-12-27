8c38ddf08d38d3cc0fc960cd86a682c68586ca30
Leporoni <leporoni@hotmail.com>
2018-12-27 15:00:01 -0200

    first commit

---
+++ src/main/java/com/leporonitech/loginspringsecurity/LoginSpringBootSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.leporonitech.loginspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringBootSecurityApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/leporonitech/loginspringsecurity/LoginSpringBootSecurityApplicationTests.java
@@ -0,0 +1,17 @@
+package com.leporonitech.loginspringsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginSpringBootSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
